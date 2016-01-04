51d72491fa4ddf673f1e351a9e96698f1b09e9a2
Mirco Bertelli <mircobe87@gmail.com>
2016-01-04 18:57:06 +0100

    added dynamic contents in to the header

--- header.php
+++ header.php
@@ -1,4 +1,13 @@
 <header class="row">
-	<div class="col-sm-6 tile">header col 0</div>
-	<div class="col-sm-6 tile">header col 1</div>
+	<div class="col-sm-2 tile">
+		logo
+	</div>
+	<div class="col-sm-10 tile">
+		<h1><?php bloginfo('name'); ?></h1>
+		<h1>
+			<small>
+				<?php bloginfo('description'); ?>
+			</small>
+		</h1>
+	</div>
 </header>
