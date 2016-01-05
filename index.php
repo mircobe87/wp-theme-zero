8948ee1708b4763cb63c8093d691b656555a2690
Mirco Bertelli <mircobe87@gmail.com>
2016-01-05 13:07:14 +0100

    implemented loop to show posts from DB

--- index.php
+++ index.php
@@ -17,15 +17,27 @@
 	</head>
 	<body class="container">
 		<?php get_header();?>
-		<div class="row" id="loop">
-			<div class="col-xs-12 tile">
-				<div class="row tile">
-					<div class="col-xs-12 col-sm-6 col-md-9 col-lg-8 tile">loop</div>
-					<div class="col-xs-12 col-sm-6 col-md-3 col-lg-4 tile">
+		<div class="row">
+					<div id="loop" class="col-xs-12 col-sm-8 col-md-9 col-lg-9 tile">
+						<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+							<article>
+								<header>
+									<?php the_title(); ?>
+								</header>
+								<div>
+									<?php the_content(); ?>
+								</div>
+								<footer>
+									<?php the_author(); ?>
+								</footer>
+							</article>
+						<?php endwhile; else : ?>
+							<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+						<?php endif; ?>
+					</div>
+					<div class="col-xs-12 col-sm-4 col-md-3 col-lg-3 tile">
 						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 					</div>
-				</div>
-			</div>
 		</div>
 		<?php get_footer();?>
 	</body>
